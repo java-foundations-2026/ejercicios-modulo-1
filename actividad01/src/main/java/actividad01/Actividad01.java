dbd52618c6d4a90b38c03530b4296a008fa43d99
christianhm97 <christian.hidalgo.martin@hotmail.com>
2026-06-29 20:45:23 +0200

    Christian Hidalgo

--- actividad01/src/main/java/actividad01/Actividad01.java
+++ actividad01/src/main/java/actividad01/Actividad01.java
@@ -1,6 +1,6 @@
 package actividad01;
 public class Actividad01 {
     public static void main(String[] args) {
-        System.out.println("Hola mundo")
+        System.out.println("Hola mundo");
     }
 }
